f12b1806932d7b7885e072631fd61ba2d0a74c3e
zjyan <1255015228@qq.com>
2022-02-16 20:52:25 +0800

    迭代1.0 具有两个前缀，迭代时无法合一，后续需要升级成共用一个前缀

--- src/main/java/jena/generate/generateOWL.java
+++ src/main/java/jena/generate/generateOWL.java
@@ -19,24 +19,37 @@ public class generateOWL {
     public static OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
     // exNs为名称空间的标识
     public static String exNs = "http://www.xidian.edu.cn/owl/ontologies/zjyan#";
+//    public static String exNs = "http://www.semanticweb.org/jeffery/ontologies/2021/5/hydro-ontology.owl#";
     // 类
     public static HashMap<String, OntClass> str2Class = new HashMap<>();
     // 实例
     public static HashMap<String, Individual> str2Individual = new HashMap<>();
     // 已经被创建的类id集合
 //    public static HashSet<String> createdNames = new HashSet<>();
+    // 创建的本体骨架
+    public static String boneOwl = "src/main/resources/output/boneOntology.owl";
+    public static String bonePrefix = "http://www.semanticweb.org/jeffery/ontologies/2021/5/hydro-ontology.owl#";
 
     public static void main(String[] args) throws IOException {
         baseData = new getBaseData();
         // 生成类 设置词条对应的解释 别名
         ObjectProperty hasAlias = m.createObjectProperty(exNs+"hasAlias");
-
+        m.read(boneOwl);
+        m.write( System.out,"RDF/XML");
         int start = 0;
-        int end = 500;
+        int end = 50;
         for(int i=start;i<end;i++){
             String name = baseData.sortWords.get(i);
-            str2Class.put(name,m.createClass(exNs + name));
-            str2Class.get(name).addLabel(baseData.jsonData.get(name).getContext(),"注释");
+            OntClass existClass =  m.getOntClass(bonePrefix+name);
+            if(existClass != null){
+                System.out.println(name);
+                str2Class.put(name,existClass);
+                str2Class.get(name).addLabel(baseData.jsonData.get(name).getContext(),"注释");
+            }else {
+                str2Class.put(name,m.createClass(exNs + name));
+                str2Class.get(name).addLabel(baseData.jsonData.get(name).getContext(),"注释");
+            }
+
             for(String alias : baseData.jsonData.get(name).getAlias()){
                 str2Class.get(name).addLabel(alias,"亦称");
             }
@@ -52,10 +65,9 @@ public class generateOWL {
         setSubClass(baseData.hyponymsRel);
         setSubClass(baseData.dictRel);
         // 设置实例
-        setIndividual(baseData.orgData,"组织机构");
-        setIndividual(baseData.perData,"人员");
-        setIndividual(baseData.termData,"水利术语");
-//        m.add(m.createResource(exNs+"测试"), hasAlias,"啥属性");
+        setIndividual(baseData.orgData,"组织");
+        setIndividual(baseData.perData,"人物");
+//        setIndividual(baseData.termData,"水利术语");
         // 写出
         wirteRdf();
     }
@@ -74,10 +86,13 @@ public class generateOWL {
 
     private static void setIndividual(LinkedList<String> data, String className) {
         for (String name:data){
-//            String name = baseData.id2name.get(id);
-            if (!str2Class.containsKey(className)){
+            OntClass boneClass = m.getOntClass(bonePrefix+className);
+            if (!str2Class.containsKey(className) && boneClass == null){
                 str2Class.put(className,m.createClass(exNs + className));
+            }else {
+                str2Class.put(className,boneClass);
             }
+
             str2Individual.put(name,m.createIndividual(exNs + name, str2Class.get(className)));
         }
     }
@@ -87,7 +102,8 @@ public class generateOWL {
         for (Map.Entry<Integer, Integer> entry:rel.entrySet()){
             String startName = baseData.id2name.get(entry.getKey());
             String endName = baseData.id2name.get(entry.getValue());
-            if(str2Class.containsKey(startName) && str2Class.containsKey(endName)){
+            OntClass endClass = m.getOntClass(bonePrefix+endName);
+            if(str2Class.containsKey(startName) && str2Class.containsKey(endName) && endClass == null){
                 str2Class.get(startName).addSubClass(str2Class.get(endName));
             }
         }
