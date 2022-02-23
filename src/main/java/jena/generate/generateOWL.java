bea182dd58b4fe6a7fe5f1ea17ccba1ccff50338
zjyan <1255015228@qq.com>
2022-02-23 21:21:30 +0800

    加入了新的关系类型和数据

--- src/main/java/jena/generate/generateOWL.java
+++ src/main/java/jena/generate/generateOWL.java
@@ -19,40 +19,46 @@ public class generateOWL {
     public static OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
     // exNs为名称空间的标识
     public static String exNs = "http://www.xidian.edu.cn/owl/ontologies/zjyan#";
-//    public static String exNs = "http://www.semanticweb.org/jeffery/ontologies/2021/5/hydro-ontology.owl#";
+    //    public static String exNs = "http://www.semanticweb.org/jeffery/ontologies/2021/5/hydro-ontology.owl#";
     // 类
     public static HashMap<String, OntClass> str2Class = new HashMap<>();
+    public static HashMap<String, OntClass> oldStr2Class = new HashMap<>();
     // 实例
     public static HashMap<String, Individual> str2Individual = new HashMap<>();
     // 已经被创建的类id集合
 //    public static HashSet<String> createdNames = new HashSet<>();
     // 创建的本体骨架
-    public static String boneOwl = "src/main/resources/output/boneOntology.owl";
+    public static String boneOwl = "src/main/resources/ontology/水利本体_v2_0.owl";
     public static String bonePrefix = "http://www.semanticweb.org/jeffery/ontologies/2021/5/hydro-ontology.owl#";
+    // 输出的位置
+    public static String outputOwl = "src/main/resources/ontology/水利本体_v2_0.owl";
 
     public static void main(String[] args) throws IOException {
         baseData = new getBaseData();
         // 生成类 设置词条对应的解释 别名
-        ObjectProperty hasAlias = m.createObjectProperty(exNs+"hasAlias");
+        ObjectProperty hasAlias = m.createObjectProperty(exNs + "hasAlias");
         m.read(boneOwl);
-        m.write( System.out,"RDF/XML");
+        m.write(System.out, "RDF/XML");
         int start = 0;
         int end = 50;
-        for(int i=start;i<end;i++){
+        for (int i = start; i < end; i++) {
             String name = baseData.sortWords.get(i);
-            OntClass existClass =  m.getOntClass(bonePrefix+name);
-            if(existClass != null){
+            OntClass existClass = m.getOntClass(bonePrefix + name);
+            if (existClass != null) {
+                // 旧类
                 System.out.println(name);
-                str2Class.put(name,existClass);
-                str2Class.get(name).addLabel(baseData.jsonData.get(name).getContext(),"注释");
-            }else {
-                str2Class.put(name,m.createClass(exNs + name));
-                str2Class.get(name).addLabel(baseData.jsonData.get(name).getContext(),"注释");
+                oldStr2Class.put(name, existClass);
+                if (existClass.getLabel("注释") == null) {
+                    oldStr2Class.get(name).addLabel(baseData.jsonData.get(name).getContext(), "注释");
+                }
+            } else {
+                str2Class.put(name, m.createClass(bonePrefix + name));
+                str2Class.get(name).addLabel(baseData.jsonData.get(name).getContext(), "注释");
+                for (String alias : baseData.jsonData.get(name).getAlias()) {
+                    str2Class.get(name).addLabel(alias, "亦称");
+                }
             }
 
-            for(String alias : baseData.jsonData.get(name).getAlias()){
-                str2Class.get(name).addLabel(alias,"亦称");
-            }
 
         }
 
@@ -60,13 +66,24 @@ public class generateOWL {
 //            String name = entity.getName();
 //            str2Class.put(name,m.createClass(exNs + name));
 //        }
+        //设置相等类关系
+        setEqualsClass(baseData.sameMeanRel);
         // 设置类的继承关系
-        setSubClass(baseData.hypernymRel);
-        setSubClass(baseData.hyponymsRel);
-        setSubClass(baseData.dictRel);
+//        setSubClassById(baseData.hypernymRel);
+//        setSubClassById(baseData.hyponymsRel);
+//        setSubClassById(baseData.dictRel);
+
+        setSubClassByName(baseData.wordInOthWordsRel);
+        setSubClassByName(baseData.tripletRel);
+        setSubClassByName(baseData.segWordsRel);
+        setSubClassByName(baseData.noFindWordsRel);
+
         // 设置实例
-        setIndividual(baseData.orgData,"组织");
-        setIndividual(baseData.perData,"人物");
+        setIndividual(baseData.orgData, "组织");
+        setIndividual(baseData.perSciData, "人物");
+        setIndividual(baseData.perHisData, "人物");
+        setIndividual(baseData.rivData, "河流");
+
 //        setIndividual(baseData.termData,"水利术语");
         // 写出
         wirteRdf();
@@ -75,36 +92,60 @@ public class generateOWL {
     private static void wirteRdf() {
         FileWriter fwriter = null;
         try {
-            fwriter = new FileWriter("E:\\idea\\jena\\src\\main\\resources\\output\\myrdf.rdf");//没有文件会自动创建
+            fwriter = new FileWriter(outputOwl);//没有文件会自动创建
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         m.write(fwriter);
-        m.write( System.out,"RDF/XML");
+        m.write(System.out, "RDF/XML");
     }
 
     private static void setIndividual(LinkedList<String> data, String className) {
-        for (String name:data){
-            OntClass boneClass = m.getOntClass(bonePrefix+className);
-            if (!str2Class.containsKey(className) && boneClass == null){
-                str2Class.put(className,m.createClass(exNs + className));
-            }else {
-                str2Class.put(className,boneClass);
+        for (String name : data) {
+            OntClass boneClass = m.getOntClass(bonePrefix + className);
+            if (!str2Class.containsKey(className) && boneClass == null) {
+                str2Class.put(className, m.createClass(exNs + className));
+            } else {
+                str2Class.put(className, boneClass);
             }
 
-            str2Individual.put(name,m.createIndividual(exNs + name, str2Class.get(className)));
+            str2Individual.put(name, m.createIndividual(exNs + name, str2Class.get(className)));
         }
     }
 
+    private static void setEqualsClass(HashMap<String, String> rel) {
+        for (Map.Entry<String, String> entry : rel.entrySet()) {
+            String startName = entry.getKey();
+            String endName = entry.getValue();
+            if (str2Class.containsKey(startName) && str2Class.containsKey(endName) && !oldStr2Class.containsKey(endName)) {
+                str2Class.get(startName).addEquivalentClass(str2Class.get(endName));
+            } else if (str2Class.containsKey(endName) && str2Class.containsKey(startName) && !oldStr2Class.containsKey(startName)) {
+                str2Class.get(endName).addEquivalentClass(str2Class.get(startName));
+            }
+        }
+    }
 
-    private static void setSubClass(HashMap<Integer, Integer> rel) {
-        for (Map.Entry<Integer, Integer> entry:rel.entrySet()){
+    private static void setSubClassByName(HashMap<String, String> rel) {
+        for (Map.Entry<String, String> entry : rel.entrySet()) {
+            String startName = entry.getKey();
+            String endName = entry.getValue();
+            if (str2Class.containsKey(startName) && str2Class.containsKey(endName) && !oldStr2Class.containsKey(endName)) {
+                str2Class.get(startName).addSubClass(str2Class.get(endName));
+            } else if (str2Class.containsKey(endName) && str2Class.containsKey(startName) && !oldStr2Class.containsKey(startName)) {
+                str2Class.get(endName).addSubClass(str2Class.get(startName));
+            }
+        }
+    }
+
+    private static void setSubClassById(HashMap<Integer, Integer> rel) {
+        for (Map.Entry<Integer, Integer> entry : rel.entrySet()) {
             String startName = baseData.id2name.get(entry.getKey());
             String endName = baseData.id2name.get(entry.getValue());
-            OntClass endClass = m.getOntClass(bonePrefix+endName);
-            if(str2Class.containsKey(startName) && str2Class.containsKey(endName) && endClass == null){
+            if (str2Class.containsKey(startName) && str2Class.containsKey(endName) && !oldStr2Class.containsKey(endName)) {
                 str2Class.get(startName).addSubClass(str2Class.get(endName));
+            } else if (str2Class.containsKey(endName) && str2Class.containsKey(startName) && !oldStr2Class.containsKey(startName)) {
+                str2Class.get(endName).addSubClass(str2Class.get(startName));
             }
         }
     }
--- src/main/java/jena/generate/getBaseData.java
+++ src/main/java/jena/generate/getBaseData.java
@@ -14,29 +14,43 @@ import java.util.Scanner;
 
 public class getBaseData {
 
-    public HashMap<String,Integer> name2id;
-    public HashMap<Integer,String> id2name;
+    public HashMap<String, Integer> name2id;
+    public HashMap<Integer, String> id2name;
     public HashMap<Integer, Entity> id2entry;
 
     public HashMap<Integer, Integer> dictRel;
     public HashMap<Integer, Integer> hypernymRel;
     public HashMap<Integer, Integer> hyponymsRel;
-    public HashMap<String,Entity> jsonData;
+    public HashMap<String, Entity> jsonData;
+
+    public HashMap<String, String> sameMeanRel;
+    public HashMap<String, String> wordInOthWordsRel;
+    public HashMap<String, String> tripletRel;
+    public HashMap<String, String> segWordsRel;
+    public HashMap<String, String> noFindWordsRel;
 
     public LinkedList<String> sortWords;
     public LinkedList<String> orgData;
-    public LinkedList<String> perData;
-    public LinkedList<String> termData;
+    public LinkedList<String> perHisData;
+    public LinkedList<String> perSciData;
+    public LinkedList<String> rivData;
 
     private String pathJson = "src/main/resources/water/水利大辞典-定义-整理数据.json";
     private String pathHypernym = "src/main/resources/water/hypernym.csv";
     private String pathHyponyms = "src/main/resources/water/hyponyms.csv";
     private String pathDictHyponyms = "src/main/resources/water/dictHyponyms.csv";
-    private String pathSortWords = "src/main/resources/water/排序词条.csv";
+    private String pathSortWords = "src/main/resources/water/排序词条3.csv";
 
     private String pathOrg = "src/main/resources/individual/水利科技-组织机构.csv";
-    private String pathPer = "src/main/resources/individual/水利史-人名.csv";
-    private String pathTerm = "src/main/resources/individual/terms.csv";
+    private String pathHisPer = "src/main/resources/individual/水利史-人名.csv";
+    private String pathSciPer = "src/main/resources/individual/水利科技-人名.csv";
+    private String pathRiv = "src/main/resources/individual/河流沟渠.csv";
+
+    private String pathSameMean = "src/main/resources/newRelation/sameMeanRel.csv";
+    private String pathWordInOthWord = "src/main/resources/newRelation/wordInOtherWords.csv";
+    private String pathTriplet = "src/main/resources/newRelation/tripletPath.csv";
+    private String pathSegWords = "src/main/resources/newRelation/segWords.csv";
+    private String pathNoFind = "src/main/resources/newRelation/noFindWords.csv";
 
     public getBaseData() throws IOException {
         name2id = new HashMap<>();
@@ -44,15 +58,24 @@ public class getBaseData {
         id2entry = new HashMap<>();
         jsonData = getJsonData(pathJson);
         sortWords = getSignalWord(pathSortWords);
+
         orgData = getSignalWord(pathOrg);
-        perData = getSignalWord(pathPer);
-        termData = getSignalWord(pathTerm);
+        perHisData = getSignalWord(pathHisPer);
+        perSciData = getSignalWord(pathSciPer);
+        rivData = getSignalWord(pathRiv);
+
         dictRel = getCsvRelationships(pathDictHyponyms);
         hypernymRel = getCsvRelationships(pathHypernym);
         hyponymsRel = getCsvRelationships(pathHyponyms);
+
+        sameMeanRel = getCsvNameRelationships(pathSameMean);
+        wordInOthWordsRel = getCsvNameRelationships(pathWordInOthWord);
+        tripletRel = getCsvNameRelationships(pathTriplet);
+        segWordsRel = getCsvNameRelationships(pathSegWords);
+        noFindWordsRel = getCsvNameRelationships(pathNoFind);
     }
 
-    public LinkedList<String> getSignalWord(String pathName){
+    public LinkedList<String> getSignalWord(String pathName) {
         LinkedList<String> signalArr = new LinkedList<>();
         // 创建scanner
         try (Scanner scanner = new Scanner(Paths.get(pathName).toFile())) {
@@ -68,11 +91,11 @@ public class getBaseData {
     }
 
     /**
-     * 读取关系
+     * 读取id类型关系
      * @return 一个关系的hashMap
      */
-    public HashMap<Integer, Integer> getCsvRelationships(String pathName){
-        HashMap<Integer,Integer> relations = new HashMap<>();
+    public HashMap<Integer, Integer> getCsvRelationships(String pathName) {
+        HashMap<Integer, Integer> relations = new HashMap<>();
         // 创建scanner
         try (Scanner scanner = new Scanner(Paths.get(pathName).toFile())) {
             // CSV文件分隔符
@@ -85,7 +108,33 @@ public class getBaseData {
                 String[] splitStr = str.split(DELIMITER);
                 String start = splitStr[0];
                 String end = splitStr[1];
-                relations.put(Integer.valueOf(start),Integer.valueOf(end));
+                relations.put(Integer.valueOf(start), Integer.valueOf(end));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return relations;
+    }
+
+    /**
+     * 读取name类型关系数据
+     * @return 返回关系
+     */
+    public HashMap<String, String> getCsvNameRelationships(String pathName) {
+        HashMap<String, String> relations = new HashMap<>();
+        // 创建scanner
+        try (Scanner scanner = new Scanner(Paths.get(pathName).toFile())) {
+            // CSV文件分隔符
+            String DELIMITER = ",";
+            // 设置分隔符
+//            scanner.useDelimiter(DELIMITER);
+            // 读取
+            while (scanner.hasNext()) {
+                String str = scanner.next();
+                String[] splitStr = str.split(DELIMITER);
+                String start = splitStr[0];
+                String end = splitStr[1];
+                relations.put(start, end);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -99,19 +148,19 @@ public class getBaseData {
      * 1.id和name的互相映射，两个map
      * 2.id和json词条的map映射，可以根据id找到entry对象
      */
-    public HashMap<String,Entity> getJsonData(String pathname) throws IOException {
-        File file=new File(pathname);
+    public HashMap<String, Entity> getJsonData(String pathname) throws IOException {
+        File file = new File(pathname);
         JsonValue jsonArr = readerMethod(file);
         JsonArray asArray = jsonArr.getAsArray();
-        HashMap<String,Entity> jsonData = new HashMap<>();
+        HashMap<String, Entity> jsonData = new HashMap<>();
         // 必要参数获取
-        for(int i=0;i<asArray.size();i++){
+        for (int i = 0; i < asArray.size(); i++) {
             Entity newEntity = transforClass(asArray.get(i).toString());
             Integer id = newEntity.getId();
             String name = newEntity.getName();
-            jsonData.put(name,newEntity);
-            id2name.put(id,name);
-            name2id.put(name,id);
+            jsonData.put(name, newEntity);
+            id2name.put(id, name);
+            name2id.put(name, id);
             id2entry.put(id, newEntity);
         }
         return jsonData;
@@ -119,6 +168,7 @@ public class getBaseData {
 
     /**
      * 读取json数据
+     *
      * @return 列表形式的json结果
      */
     private JsonValue readerMethod(File file) throws IOException {
@@ -138,7 +188,7 @@ public class getBaseData {
     /**
      * 将json字符串形式数据解析成entry对象
      */
-    private Entity transforClass(String jsonStr){
+    private Entity transforClass(String jsonStr) {
         //JSON-LIB
         JSONObject jsonResult = JSONObject.fromObject(jsonStr);
         Entity entity = (Entity) JSONObject.toBean(jsonResult, Entity.class);
